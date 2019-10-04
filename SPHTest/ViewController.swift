f92431ed1cbaf811ec4b4df38332746b660a16c4
xuanzi <lawlierxuanzi@gmail.com>
2019-10-04 15:49:25 +0800

    Initial Commit

---
+++ SPHTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPHTest
+//
+//  Created by YAP HAO XUAN on 04/10/2019.
+//  Copyright © 2019 YAP HAO XUAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
